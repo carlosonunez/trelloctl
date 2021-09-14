e3dade94d031f2027f41958f4b2291e07e6bc4b6
Carlos Nunez <carlosonunez@noreply.github.com>
2021-09-14 14:31:09 -0500

    NEW COMMAND: Show labels associated with one or more boards

---
+++ list_labels.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'thread/pool'
+require 'trello'
+require 'trelloctl'
+require 'yaml'
+
+PROGRAM_BANNER = "#{$PROGRAM_NAME}: Lists labels associated with one or more boards"
+
+options = Options.new(PROGRAM_BANNER)
+options.add_option(long_flag: '--boards',
+                   description: 'The boards from which the label should be removed',
+                   type: Array,
+                   id: 'boards',
+                   required: true)
+options.add_option(long_flag: '--output',
+                   description: 'Output format',
+                   id: 'format',
+                   default: 'yaml',
+                   acceptable: %w[yaml json])
+options.gather!(ARGV)
+options.exit_if_usage_set!
+options.exit_if_missing_required_vars!
+
+boards = options.value(:boards)
+user = TrelloUser.instance
+
+boards_pool = Thread.pool(5)
+labels = []
+user.filter_boards(boards).each do |board_ref|
+  boards_pool.process do
+    user.with_retries { Trello::Board.find(board_ref.id) }.labels.map do |label|
+      labels << {
+        label: {
+          name: label.name,
+          id: label.id,
+          board: {
+            name: board_ref.name,
+            id: board_ref.id
+          }
+        }
+      }
+    end
+  end
+end
+boards_pool.shutdown
+json = labels.to_json
+puts YAML.safe_load(json).to_yaml unless options.value(:format) == 'json'
+puts labels.to_json unless options.value(:format) == 'yaml'
